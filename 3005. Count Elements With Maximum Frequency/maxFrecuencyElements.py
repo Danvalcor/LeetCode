9a98fd786f58d0b853bebe64358d37f2c97e0fd3
Danvalcor <Danielvalenzuela.2001@gmail.com>
2024-03-08 11:00:04 -0600

    Update maxFrecuencyElements.py

--- 3005. Count Elements With Maximum Frequency/maxFrecuencyElements.py
+++ 3005. Count Elements With Maximum Frequency/maxFrecuencyElements.py
@@ -1,10 +1,10 @@
 class Solution:
     def maxFrequencyElements(self, nums: list[int]) -> int:
-        dict = {}
-        for i in nums:
-            dict[i] = dict.get(i, 0) + 1
-        maxFreq = max(dict.values())
-        return sum(maxFreq == n for n in dict.values())*maxFreq
+        dict = {}                           # Se crea una hashtable para guardar las frecuencias
+        for i in nums:                      
+            dict[i] = dict.get(i, 0) + 1    # Se actualiza la frecuencia con la cual aparece cada numero.
+        maxFreq = max(dict.values())        # Se obtiene la frecuencia más alta.
+        return sum(maxFreq == n for n in dict.values())*maxFreq # Se cuentan los numeros con la misma frecuencia y se multiplican.
 
 
 
